ad5e60ac1d181be7b8cc7cbc4a5288322ca47e70
Kevin Chau <Kevin Chau@DESKTOP-TA19N7F>
2024-10-17 22:36:28 -0400

    Changed schema to include tasks. Also able to get authenticated users' tasks

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,21 +1,21 @@
 const User = require("../model/User")
 
-/*
-const UserSchema = new Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-})
-*/
-
-
-//in order to create a task you need to reference the
 exports.createUser = async(req, res) => {
+
     const newUser = new User({
         username: req.body.username,
-        password: req.body.password
+        password: req.body.password,
+        tasks: [
+            {
+            task_name: 'Wash car',
+            completed: false,
+            created: new Date(),
+            completedAt: null
+            }
+        ]
     })
     try{
-        newUser.save();
+        await newUser.save();
         res.status(201).json({message: 'Created user!'});
     }catch(err){
         console.error('Error creating user!', err);
--- server/model/User.js
+++ server/model/User.js
@@ -4,8 +4,16 @@ const {Schema, model} = require('mongoose');
 const UserSchema = new Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    tasks: [
+        {
+            task_name: { type: String, required: true },
+            completed: {type: Boolean, default: false},
+            created: {type: Date, default: Date.now},
+            completedAt: {type: Date, default: null}
+        }
+    ]
 })
 
-const User = model('User', UserSchema);
+const User = model('Users', UserSchema);
 
 module.exports = User;
