f0c3ac79e92f81bec74f26cd2e8e33b279a0bdd9
KEVIN CHAU <kevinch@udel.edu>
2024-10-23 02:51:26 -0400

    Changed to use jwt

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,5 +1,41 @@
 const User = require("../model/User")
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, "../.env") })
+const JWT_SECRET = process.env.JWT_SECRET
+
+
+/*
+We want to take the username and password out of the request body
+Then find the user.
+If the user doesn't exist, send an error.
+If the password isn't correct, send an error.
+Otherwise, generate a token and send that back to the browser.
+*/
+
+exports.login = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.findOne({ username: username });
+
+        if (!user) {
+            return res.status(403).json({ message: 'User does not exist!' });
+        }
+        const isMatchedPassword = await bcrypt.compare(password, user.password);
+        if (!isMatchedPassword) {
+            return res.status(403).json({ message: "Password is incorrect!" })
+        }
+
+        const token = jwt.sign({ user: user }, JWT_SECRET);
+        res.status(201).json({ token: token, username: username });
+
+    } catch (err) {
+        console.error('Error when logging in', err);
+        return res.status(500).json({ message: "Server error when logging in!" });
+    }
+}
 
 exports.createUser = async (req, res) => {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
@@ -15,7 +51,7 @@ exports.createUser = async (req, res) => {
         return res.status(201).json({ message: 'Created user!' });
     } catch (err) {
         console.error('Error creating user!', err);
-        return res.status(500).json({message: 'Server error when creating a user!'});
+        return res.status(500).json({ message: 'Server error when creating a user!' });
     }
 }
 /*
@@ -27,50 +63,40 @@ exports.createUser = async (req, res) => {
 
 */
 exports.addTask = async (req, res) => {
-    if (req.isAuthenticated()) {
-        try {
-            const task_name = req.body.task_name;
-            const userID = req.user._id;
-            const newTask = {
-                task_name: task_name,
-                completed: false,
-                created: new Date(),
-                completedAt: null
-            }
-            await User.updateOne(
-                { _id: userID },
-                { $push: { tasks: newTask } }
-            )
-            return res.status(201).json({message: 'successful'});
-
-        } catch (err) {
-            console.error(err);
-            return res.status(500).json({message: 'Server error when inserting task!'})
+    try {
+        const task_name = req.body.task_name;
+        const userID = req.user;
+        const newTask = {
+            task_name: task_name,
+            completed: false,
+            created: new Date(),
+            completedAt: null
         }
-    }
-    else {
-        return res.status(403).send('You have been logged out!');
+        await User.updateOne(
+            { _id: userID },
+            { $push: { tasks: newTask } }
+        )
+        return res.status(201).json({ message: 'successful' });
+
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Server error when inserting task!' })
     }
 }
 
-exports.retrieveTasks = async(req, res) => {
-    if (req.isAuthenticated()){
-        const userID = req.user._id;
-        try{
-            const userTasks = await User.findById(userID);
-            return res.status(200).json({
-                userTasks: userTasks.tasks
-            })
-            
-        }catch(err){
-            console.error('Error retrieving user tasks', err);
-            return res.status(500).json({
-                message: 'Server error when retrieving tasks!'
-            })
-        }
-    }
-    else{
-        return res.status(403).json({message: 'You have been logged out!'});
+exports.retrieveTasks = async (req, res) => {
+    const userID = req.user;
+    try {
+        const userTasks = await User.findById(userID);
+        return res.status(200).json({
+            userTasks: userTasks.tasks
+        })
+
+    } catch (err) {
+        console.error('Error retrieving user tasks', err);
+        return res.status(500).json({
+            message: 'Server error when retrieving tasks!'
+        })
     }
 }
 /*
@@ -84,27 +110,22 @@ If user is authenticated
     - look for the corresponding taskID, and update the task with whatever is in req.body.task_name;
 
 */
-exports.updateTaskName = async(req, res) => {
-    if (req.isAuthenticated()){
-        const userID = req.user._id;
-        const new_task_name = req.body.new_task_name;
-        const task_id = req.body.task_id;
-        try{
-            //we have the user now.
-            const user = await User.findById(userID);
-            //go through the user's tasks, and find where the task id is equal to the taskID passed in
-            const foundTask = user.tasks.find((task) => task._id.toString() == task_id)
-            foundTask.task_name = new_task_name;
+exports.updateTaskName = async (req, res) => {
+    const userID = req.user;
+    const new_task_name = req.body.new_task_name;
+    const task_id = req.body.task_id;
+    try {
+        //we have the user now.
+        const user = await User.findById(userID);
+        //go through the user's tasks, and find where the task id is equal to the taskID passed in
+        const foundTask = user.tasks.find((task) => task._id.toString() == task_id)
+        foundTask.task_name = new_task_name;
 
-            await user.save();
-            return res.status(201).json({message: 'successful!'});
-        }catch(err){
-            console.error('Error updating task!', err)
-            return res.status(500).json({message: 'failure'})
-        }
-    }
-    else{
-        return res.status(403).send('You have been logged out!');
+        await user.save();
+        return res.status(201).json({ message: 'successful!' });
+    } catch (err) {
+        console.error('Error updating task!', err)
+        return res.status(500).json({ message: 'failure' })
     }
 }
 
@@ -120,24 +141,21 @@ Approach:
 
 */
 
-exports.deleteTask = async(req, res) => {
-    if (req.isAuthenticated()){
-        const userID = req.user._id
-        const task_id = req.body.task_id;
-        try{
-            const user = await User.findById(userID);
-            //Filter out all the tasks that do not have the task_id that we passed in
-            const newTasks = user.tasks.filter((task) => task._id.toString() !== task_id);
-            user.tasks = newTasks;
-            user.save();
-            return res.json({message: 'successful'});
+exports.deleteTask = async (req, res) => {
+    const userID = req.user
+    const task_id = req.body.task_id;
+    try {
+        const user = await User.findById(userID);
+        //Filter out all the tasks that do not have the task_id that we passed in
+        const newTasks = user.tasks.filter((task) => task._id.toString() !== task_id);
+        user.tasks = newTasks;
+        user.save();
+        return res.json({ message: 'successful' });
 
-        }catch(err){
-            console.error('Error deleting task!', err);
-            return res.status(500).json({message: 'failure'})
-        }
+    } catch (err) {
+        console.error('Error deleting task!', err);
+        return res.status(500).json({ message: 'failure' })
     }
-    return res.status(403).json({message: 'Authorization Error: You are not signed in!'})
 }
 
 /*
@@ -149,24 +167,19 @@ exports.deleteTask = async(req, res) => {
 */
 
 
-exports.changeCompletion = async(req, res) => {
-    if (req.isAuthenticated()){
-        const userID = req.user._id;
-        const task_id = req.body.task_id;
+exports.changeCompletion = async (req, res) => {
+    const userID = req.user;
+    const task_id = req.body.task_id;
 
-        try{
-            const user = await User.findById(userID);
-            const queriedTask = user.tasks.find((task) => task._id.toString() === task_id);
-            queriedTask.completed = !queriedTask.completed; //this will take the current status of the completion and flip it.
-            await user.save();
-            return res.status(200).json({message: 'successful'})
-        }
-        catch(err){
-            console.error('Error changing completion', err);
-            return res.status(400).json({message: 'failed'})
-        }
+    try {
+        const user = await User.findById(userID);
+        const queriedTask = user.tasks.find((task) => task._id.toString() === task_id);
+        queriedTask.completed = !queriedTask.completed; //this will take the current status of the completion and flip it.
+        await user.save();
+        return res.status(200).json({ message: 'successful' })
     }
-    else{
-        return res.status(403).json({message: 'Authorization Failure!'})
+    catch (err) {
+        console.error('Error changing completion', err);
+        return res.status(400).json({ message: 'failed' })
     }
 }
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,14 +1,30 @@
 const { Router } = require('express');
-const { createUser, addTask, retrieveTasks, updateTaskName, deleteTask, changeCompletion } = require('../controllers/controller');
-const passport = require('passport');
-const User = require('../model/User');
+const { createUser, addTask, retrieveTasks, updateTaskName, deleteTask, changeCompletion, login } = require('../controllers/controller');
 const apiRouter = Router();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, "../.env") })
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = process.env.JWT_SECRET
 
+//Check the headers authorization field and then if it exists, split the token between Bearer and the token
+const auth = async(req, res,next) => {
+    const bearer = req.headers.authorization;
+    if (!bearer){
+        return res.status(403).json({message: 'You are not currently signed in!'});
+    }
+    else{
+        const token = bearer.split(' ')[1];
+        req.token = token;
+        const decoded = jwt.verify(req.token, JWT_SECRET);
+        req.user = decoded.user._id;
+        next();
+    }
+}
 
 
 // ------------------- GET ROUTES ----------------------------
 
-apiRouter.get('/tasks', retrieveTasks);
+apiRouter.get('/tasks', auth, retrieveTasks);
 
 apiRouter.get('/logout', (req, res) => {
     req.logout((err) => {
@@ -22,32 +38,17 @@ apiRouter.get('/logout', (req, res) => {
 // ------------------ START OF POST ROUTES ----------------------------
 
 //waits for post at login route, then passport will authenticate as middleware, then if successful, we move  onto the next middleware (3rd param)
-apiRouter.post('/login', passport.authenticate('local'), (req, res) => {
-    if (req.user){
-        res.status(200).json({
-            status: "success",
-            _id: req.user._id,
-            username: req.user.username,
-        })
-    }
-    else{
-        res.status(401).json({
-            status: "failure",
-            message: "Invalid username or password!"
-        })
-    }
-}
-)
-apiRouter.post('/addTask', addTask);
-apiRouter.post('/register', createUser);
+apiRouter.post('/login', login);
+apiRouter.post('/addTask', auth, addTask);
+apiRouter.post('/register', auth, createUser);
 
 // ---------------- START OF PUT ROUTES ----------------------------------
-apiRouter.put('/tasks/updateTaskName', updateTaskName);
-apiRouter.put('/tasks/changeCompletion', changeCompletion)
+apiRouter.put('/tasks/updateTaskName', auth, updateTaskName);
+apiRouter.put('/tasks/changeCompletion', auth, changeCompletion)
 
 
 // ----------------- START OF DELETE ROUTES -------------------------------
-apiRouter.delete('/tasks/delete', deleteTask)
+apiRouter.delete('/tasks/delete', auth, deleteTask)
 
 
 
